24ec5f9d784a5e73bb825cb030b44ecd7b50c4d1
Omar Castro <ocstro@gmail.com>
2016-05-20 16:22:39 +0000

    first commit

---
+++ lecture.js
@@ -0,0 +1,27 @@
+// this is a comment 
+
+/* 
+    you can type comments here 
+*/
+
+/*var foo = 1; 
+var iLikeCamels = 1.1;
+var snakes_are_awesome;
+var _singleton;
+
+console.log(foo)
+
+var math = (5+2)*2;
+
+console.log(math)
+*/
+
+var d = "don't";
+var t = 'taze';
+var m = 'me';
+var b = 'bro';
+
+var sentence2 = d + ' ' +t+ ' ' +m ''+b;
+
+console.log(sentence2)
+
